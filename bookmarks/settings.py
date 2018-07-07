bb12a70cfc8b9b5aef690708ffaddf571bc55b3f
summericeyl <summericeyl@gmail.com>
2018-07-08 01:51:21 +0800

    设置数据库

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +73,20 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BOOKMARKS',
+        'USER': 'root',
+        'PASSWORD': 'xhyn723',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
